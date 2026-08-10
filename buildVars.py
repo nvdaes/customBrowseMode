24db12d69fb07b21885f2ad7be044aa47957eabc
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-08-10 18:32:50 +0200

    Update changelog

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = AddonInfo(
 	addon_version='6.0.0',
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_('* Compatible with NVDA 2026.1.'),
+	addon_changelog=_("* Compatible with NVDA 2026.1."),
 	# Author(s)
 	addon_author='Noelia Ruiz Martínez <nrm1977@gmail.com>, Ever Solis <disoever@gmail.com>',
 	# URL for the add-on documentation support
